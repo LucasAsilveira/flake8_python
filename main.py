2d2e84c3454478ddaf2434fa098b9c77a7212f08
LucasAsilveira <lucasaspal@gmail.com>
2022-04-28 10:18:13 -0300

    creating fila base

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fila_normal import filanormal
+from fila_normal import FilaNormal
 from fila_prorioritaria import FilaPrioritaria
 
 
@@ -8,14 +8,4 @@ fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 print(fila_teste_2.chama_cliente(10))
 print(fila_teste_2.chama_cliente(3))
-print(fila_teste_2.estatistica('10/01/1993', 197, 'detail'))
-
-
-# fila_teste = filanormal()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# print(fila_teste.chamacliente(5))
-# print(fila_teste.chamacliente(10))
-# print(fila_teste.chamacliente(8))
+print(fila_teste_2.estatistica('10/01/1993', '215', 'detail'))
